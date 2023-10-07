35d09d8dedab6389081ca8f56157eda5145e65b2
stokeydev <120173316+stokeydev@users.noreply.github.com>
2023-10-07 15:05:17 +0100

    change test

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,7 +2,7 @@ function ListGroup() {
   return (
     <ul className="list-group">
       <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A 2nd item</li>
       <li className="list-group-item">A third item</li>
       <li className="list-group-item">A fourth item</li>
       <li className="list-group-item">And a fifth one</li>
